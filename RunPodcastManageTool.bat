60f926d5f81e7722ac2852e97139403bfc15a58c
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2025-02-04 00:55:10 -0600

    base batch scripts

---
+++ RunPodcastManageTool.bat
@@ -0,0 +1,3 @@
+@echo off
+@REM Run the config script. Ideally all management tasks should be done with this tool
+python  ".\podcast_manage_tool.py"
---
+++ RunScript.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM Run the main script. This will get configs the from the
+@REM _playlist and_username files and fetch all new videos
+python ".\refresh-podcasts.py"
